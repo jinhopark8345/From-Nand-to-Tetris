399e65948b9604a44cde80adfd7695e3e1dda693
jinho <jinhopark8345@gmail.com>
2021-02-10 02:22:33 +0900

    last commit

--- projects/08/VMTranslator/VMTranslator.py
+++ projects/08/VMTranslator/VMTranslator.py
@@ -7,16 +7,37 @@ if __name__ == '__main__':
   # asm_file = os.path.splitext(vm_file)[0] + '.asm'
   # lines = VMParser(sys.argv[1]).get_commands()
 
-  vm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/ProgramFlow/BasicLoop/BasicLoop.vm'
-  asm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/ProgramFlow/BasicLoop/BasicLoop.asm'
+  # vm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/FibonacciElement/Sys.vm'
+  # # vm_file2 = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/FibonacciElement/Main.vm'
+  # asm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/FibonacciElement/FibonacciElement.asm'
 
+  # vm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/NestedCall/Sys.vm'
+  # asm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/NestedCall/NestedCall.asm'
   # vm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/ProgramFlow/FibonacciSeries/FibonacciSeries.vm'
   # asm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/ProgramFlow/FibonacciSeries/FibonacciSeries.asm'
+  # from pathlib import Path
+  # temp = Path(asm_file).parent
+  # print(str(temp))
 
+  vm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/StaticsTest/Sys.vm'
+  vm_file2 = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/StaticsTest/Class1.vm'
+  vm_file3 = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/StaticsTest/Class2.vm'
+  asm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/StaticsTest/StaticsTest.asm'
+
+# Returns a Pathlib object
 
   lines = VMParser(vm_file).get_commands()
+  lines.extend(VMParser(vm_file2).get_commands())
+  lines.extend(VMParser(vm_file3).get_commands())
+  CodeWriter(asm_file, lines)
 
   # print(lines)
   # for line in lines:
     # print(line)
-  CodeWriter(asm_file, lines)
+
+    # func_path = '{}/{}.vm'.format(str(Path(self.output_file).parent), (line[1].split(sep='.')[0]))
+    # # print(func_path)
+    # func_lines = VMParser(func_path).get_commands()
+    # # print(func_lines)
+    # for func_line in func_lines:
+    #   self.write_line(func_line)
--- projects/08/VMTranslator/vm_codewriter.py
+++ projects/08/VMTranslator/vm_codewriter.py
@@ -4,6 +4,7 @@ writes the assembly code that implements the parsed command"""
 # from ram import Ram
 
 from vm_parser import VMParser
+from pathlib import Path
 
 
 class CodeWriter:
@@ -16,8 +17,10 @@ class CodeWriter:
     self.lines = lines
     self.if_count = 0
     self.line_count = 0
-    self.command_type = set(['push', 'pop', 'label', 'if-goto',
-                         'goto', 'call', 'return', 'function'])
+    self.return_count = 0
+    self.command_type = set([
+        'push', 'pop', 'label', 'if-goto', 'goto', 'call', 'return', 'function'
+    ])
     self.labels = set()
     self.assem_lines = []
 
@@ -28,64 +31,204 @@ class CodeWriter:
 
     self.f.close()
 
+  def write_line(self, line):
+    print("\t\t<current line: {}>".format(line))
+    command_type = line[0]
+    if command_type in self.command_type:
+      if command_type in ('push', 'pop'):
+        self.write_push_pop(line)
+      elif command_type == 'label':
+        self.save_label(line)
+      elif command_type in ('if-goto', 'goto'):
+        self.write_goto(line)
+      elif command_type == 'function':
+        self.write_function(line)
+      elif command_type == 'call':
+        self.write_call(line)
+      elif command_type == 'return':
+        self.write_return(line)
+      else:
+        print(f"undefined command type {command_type}")
+    else:  # arithmetic command
+      self.write_arithmetic(command_type)
+
   def init_code_writer(self):
 
-    self.assem_lines.append('@256')
+    # self.assem_lines.append('@256')
+    self.assem_lines.append('@261')
     self.assem_lines.append('D=A')
     self.assem_lines.append('@0')
     self.assem_lines.append('M=D')
 
-    self.assem_lines.append('@300')
-    self.assem_lines.append('D=A')
-    self.assem_lines.append('@1')
-    self.assem_lines.append('M=D')
+    # self.assem_lines.append('@300')
+    # self.assem_lines.append('D=A')
+    # self.assem_lines.append('@1')
+    # self.assem_lines.append('M=D')
 
-    self.assem_lines.append('@400')
-    self.assem_lines.append('D=A')
-    self.assem_lines.append('@2')
-    self.assem_lines.append('M=D')
+    # self.assem_lines.append('@400')
+    # self.assem_lines.append('D=A')
+    # self.assem_lines.append('@2')
+    # self.assem_lines.append('M=D')
+    # goto f, add vm function code
 
-    self.assem_lines.append('@3000')
-    self.assem_lines.append('D=A')
-    self.assem_lines.append('@3')
-    self.assem_lines.append('M=D')
+    # self.assem_lines.append('@3000')
+    # self.assem_lines.append('D=A')
+    # self.assem_lines.append('@3')
+    # self.assem_lines.append('M=D')
 
-    self.assem_lines.append('@3010')
-    self.assem_lines.append('D=A')
-    self.assem_lines.append('@4')
-    self.assem_lines.append('M=D')
+    # self.assem_lines.append('@3010')
+    # self.assem_lines.append('D=A')
+    # self.assem_lines.append('@4')
+    # self.assem_lines.append('M=D')
 
     # for ass_line in self.assem_lines:
     #   self.f.writelines(ass_line + '\n')
 
     for line in self.lines:
-      # if self.line_count == 31:
+      # if self.line_count == 15:
       #   break
       # self.line_count += 1
 
-      print("current line: {}".format(line))
-      # self.ram.process_line(line)
+      last_ass_line_num = len(self.assem_lines)
+      self.write_line(line)
+      cur_ass_line_num = len(self.assem_lines)
 
-      command_type = line[0]
-      if command_type in self.command_type:
-        if command_type in ('push', 'pop'):
-          self.write_push_pop(line)
-        if command_type == 'label':
-          self.save_label(line)
-        if command_type in ('if-goto', 'goto'):
-          self.write_goto(line)
-      else:  # arithmetic command
-        self.write_arithmetic(command_type)
+      for i in range(last_ass_line_num, cur_ass_line_num):
+        print('{}\t{}'.format(i, self.assem_lines[i]))
+      # print('\n'.join(self.assem_lines[last_ass_line_num:cur_ass_line_num]))
 
-    self.assem_lines.append('(INFINITE_LOOP)')
-    self.assem_lines.append('@INFINITE_LOOP')
-    self.assem_lines.append('0;JMP')
+    # self.assem_lines.append('(INFINITE_LOOP)')
+    # self.assem_lines.append('@INFINITE_LOOP')
+    # self.assem_lines.append('0;JMP')
 
     for ass_line in self.assem_lines:
       self.f.writelines(ass_line + '\n')
       # self.vm_debugger()
   def save_label(self, line):
     self.assem_lines.append(f'({line[1]})')
+
+  def write_function(self, line):
+    func_name = line[1]
+    nargs = int(line[2])
+    # (f)
+    self.assem_lines.append('({})'.format(func_name))
+
+    # repeat k times: push 0
+    if nargs > 0:
+      for _ in range(nargs):
+        self.write_push_pop(['push', 'constant', 0])
+
+  def write_return(self, _):
+    assem_pop = ['@SP', 'M=M-1', 'A=M', 'D=M']
+    # FRAME = LCL, *(@13) = FRAME
+    self.assem_lines.append('@LCL')
+    self.assem_lines.append('D=M')
+    self.assem_lines.append('@13')
+    self.assem_lines.append('M=D')
+
+    # RET = *(FRAME-5), *(@14) = RET
+    self.assem_lines.append('@5')
+    self.assem_lines.append('D=D-A')
+    self.assem_lines.append('A=D')
+    self.assem_lines.append('D=M')
+    self.assem_lines.append('@14')
+    self.assem_lines.append('M=D')
+
+    # *ARG = pop()
+    self.assem_lines.extend(assem_pop)
+    self.assem_lines.append('@ARG')
+    self.assem_lines.append('A=M')
+    self.assem_lines.append('M=D')  # *(@2) = pop()
+
+    # SP = ARG+1
+    self.assem_lines.append('@ARG')
+    self.assem_lines.append('D=M+1')  # D = ARG+1
+    self.assem_lines.append('@SP')
+    self.assem_lines.append('M=D')  # SP = D(=ARG+1)
+
+    # THAT = *(FRAME-1)
+    self.assem_lines.append('@13')
+    self.assem_lines.append('M=M-1')  # M = FRAME-1, *(@13) = FRAME-1
+    self.assem_lines.append('D=M')  # D = FRAME-1
+    self.assem_lines.append('A=D')
+    self.assem_lines.append('D=M')  # D = *(FRAME-1)
+    self.assem_lines.append('@THAT')
+    self.assem_lines.append('M=D')
+
+    # THIS = *(FRAME-2)
+    self.assem_lines.append('@13')
+    self.assem_lines.append('M=M-1')  # M = FRAME-2, *(@13) = FRAME-2
+    self.assem_lines.append('D=M')  # D = FRAME-2
+    self.assem_lines.append('A=D')
+    self.assem_lines.append('D=M')  # D = *(FRAME-2)
+    self.assem_lines.append('@THIS')
+    self.assem_lines.append('M=D')
+
+    # ARG = *(FRAME-3)
+    self.assem_lines.append('@13')
+    self.assem_lines.append('M=M-1')  # M = FRAME-3, *(@13) = FRAME-3
+    self.assem_lines.append('D=M')  # D = FRAME-3
+    self.assem_lines.append('A=D')
+    self.assem_lines.append('D=M')  # D = *(FRAME-3)
+    self.assem_lines.append('@ARG')
+    self.assem_lines.append('M=D')
+
+    # LCL = *(FRAME-4)
+    self.assem_lines.append('@13')
+    self.assem_lines.append('M=M-1')  # M = FRAME-4, *(@14) = FRAME-4
+    self.assem_lines.append('D=M')  # D = FRAME-4
+    self.assem_lines.append('A=D')
+    self.assem_lines.append('D=M')  # D = *(FRAME-4)
+    self.assem_lines.append('@LCL')
+    self.assem_lines.append('M=D')
+
+    # goto RET
+    self.assem_lines.append('@14')
+    self.assem_lines.append('A=M')
+    self.assem_lines.append('0;JMP')
+
+  def write_call(self, line):
+
+    assem_push = ['@SP', 'A=M', 'M=D', '@SP', 'M=M+1']
+    # assem_pop = ['@SP', 'M=M-1', 'A=M', 'D=M']
+    func = line[1]
+    nargs = int(line[2])
+
+    self.assem_lines.append('@{}$RETURN_ADDRESS{}'.format(func, self.return_count))
+
+    self.assem_lines.append('D=A')  # not sure if this is right
+    self.assem_lines.extend(assem_push)
+
+    # push LCL, ARG, THIS, THAT
+    for i in range(1, 5):
+      self.assem_lines.append('@{}'.format(i))
+      self.assem_lines.append('D=M')
+      self.assem_lines.extend(assem_push)
+
+    # reposition ARG, ARG = SP-n-5
+    self.assem_lines.append('@SP')
+    self.assem_lines.append('D=M')  # D = SP
+    self.assem_lines.append('@{}'.format(nargs))
+    self.assem_lines.append('D=D-A')  # D = SP - n
+    self.assem_lines.append('@5')
+    self.assem_lines.append('D=D-A')  # D = SP - n - 5
+    self.assem_lines.append('@ARG')
+    self.assem_lines.append('M=D')  # ARG = SP - n - 5
+
+    # reposition LCL, LCL = SP
+    self.assem_lines.append('@SP')
+    self.assem_lines.append('D=M')
+    self.assem_lines.append('@LCL')
+    self.assem_lines.append('M=D')  # LCL = SP
+
+    # goto f, add vm function code
+    self.assem_lines.append('@{}'.format(func))
+    self.assem_lines.append('0;JMP')
+
+    # (return-address)
+    self.assem_lines.append('({}$RETURN_ADDRESS{})'.format(func, self.return_count))
+    self.return_count += 1
+
   def write_goto(self, line):
 
     assem_pop = ['@SP', 'M=M-1', 'A=M', 'D=M']
@@ -175,7 +318,7 @@ class CodeWriter:
       self.assem_lines.append('(if_true{})'.format(self.if_count))
       self.assem_lines.append('@SP')
       self.assem_lines.append('A=M')
-      self.assem_lines.append('M=-1') # true
+      self.assem_lines.append('M=-1')  # true
       self.assem_lines.append('@SP')
       self.assem_lines.append('M=M+1')
       self.assem_lines.append('@if_end{}'.format(self.if_count))
@@ -184,7 +327,7 @@ class CodeWriter:
       self.assem_lines.append('(if_false{})'.format(self.if_count))
       self.assem_lines.append('@SP')
       self.assem_lines.append('A=M')
-      self.assem_lines.append('M=0') # false
+      self.assem_lines.append('M=0')  # false
       self.assem_lines.append('@SP')
       self.assem_lines.append('M=M+1')
 
@@ -193,7 +336,6 @@ class CodeWriter:
       self.if_count += 1
     else:
       print("undefined arithmetic input")
-
 
   def write_push_pop(self, line):
     command = line[0]
@@ -321,5 +463,14 @@ if __name__ == '__main__':
 
   vm_file = '/home/jinho/Dropbox/Projects/FromNandToTetris/projects/07/StackArithmetic/StackTest/StackTest.vm'
   output_file = '/home/jinho/Dropbox/Projects/FromNandToTetris/projects/07/StackArithmetic/StackTest/StackTest.asm'
+
+  vm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/ProgramFlow/FibonacciSeries/FibonacciSeries.vm'
+
+  output_file = '/home/jinho/Projects/FromNandToTetris/projects/08/ProgramFlow/FibonacciSeries/FibonacciSeries.asm'
+
+  vm_file = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/SimpleFunction/SimpleFunction.vm'
+
+  output_file = '/home/jinho/Projects/FromNandToTetris/projects/08/FunctionCalls/SimpleFunction/SimpleFunction.asm'
+
   lines = VMParser(vm_file).get_commands()
   cw = CodeWriter(output_file, lines)
